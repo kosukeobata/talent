1e3eaa545a706aa682f07ff90f5256d254f8db0c
kosukeobata <kosuke.obata@bizreach.co.jp>
2017-08-05 15:07:31 +0900

    Initial Commit.

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var models = require("../models");
+
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var user_id = req.query.user_id;
+  var name = req.query.name;
+	console.log('user_id', user_id);
+	console.log('name', name);
+  models.User.findOrCreate(
+    {"where": {"id": user_id}, 
+     "defaults": {"id": user_id, "name": name}}
+   ).spread(function(user, created){
+      res.send({"user": user, "created": created});
+   });
+});
+
+module.exports = router;
